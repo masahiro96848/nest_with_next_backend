52aee0d7c873ab95b1ba0fdf68eea87c9b97d91c
masahiro96848 <feetwcpic9436@gmail.com>
2023-03-21 17:16:06 +0900

    userのserviceとcontrolerを実装。strategyディレクトリを作成

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  /**
+   * ユーザーのニックネームを更新
+   * @param userId
+   * @param dto
+   * @returns user
+   */
+  async updateUser(
+    userId: number,
+    dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+    delete user.hashedPassword;
+    return user;
+  }
+}
